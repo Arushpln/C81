8247eca2d93439e364c1dca5709693ad9aff1cab
Arushpln <84733127+Arushpln@users.noreply.github.com>
2022-09-28 17:59:30 +0100

    Add files via upload

---
+++ Profile.js
@@ -0,0 +1,20 @@
+import React ,{component} from 'react';
+import {
+    Text,
+    View
+} from 'react-native';
+
+export default class Profile extends Component{
+      render(){
+        return(
+            <View
+            style = {{
+                flex:1,
+                justifyContent:'center',
+                alignItems:'center'
+            }}>
+               <Text>Profile</Text> 
+            </View>
+        )
+      }
+} 
